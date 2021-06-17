6d21471376adc60b1f24cf1a617f5e56dca98af2
Gabi <ggonzalez@npsstore.net>
2021-06-16 19:50:13 -0600

    this is my first commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,26 @@
+import maintest.Base;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test1 extends Base {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = initializeDriver();
+    }
+
+    @Test
+    public void testHelloWorld(){
+        driver.get("https://rpp.pe/");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+
+
+}
